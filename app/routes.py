b2e79140ec5ef3542710f7ca2e1c0e8c96202a3a
Ian Rackow <ian.rackow@gmail.com>
2020-04-20 01:58:39 -0400

    Initial

---
+++ app/routes.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+from scipy.optimize import curve_fit
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/test', methods=['GET'])
+def trial():
+    return render_template("trial.html")
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    results = request.form['results'].split(",")
+
+    vals = list(map(int, results[::4]))
+    probs = list(map(float, results[1::4]))
+    uppers = list(map(int, results[2::4]))
+    lowers = list(map(int, results[3::4]))
+
+    # compute expected certainty ratios
+    cx = list(map(lambda x,y,z:(x+y)/(2*z), uppers, lowers, vals))
+
+    points = str(list(map(lambda x,y:[x, y], probs, cx)))
+
+    gamma = weight_regression(probs, cx)
+
+    print(probs, cx, gamma)
+
+    return render_template("results.html", gamma=gamma, data=points)
+
+
+# Run least squares regression using the TK 1992 functional form
+def weight_regression(probs, weights):
+
+    popt, pcov = curve_fit(tk, probs, weights, bounds=(0, 1))
+
+    print(popt, pcov)
+
+    return popt[0]
+
+
+# Parametric Weighting function
+def tk(x, g):
+
+    return (x**g) / ((x**g + (1 - x)**g)**(1/g))
+
+if __name__ == '__main__':
+    app.run()
